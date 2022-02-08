4db6074e29f821a8f29f1eae04d1f0156cf6a6a6
KsoraCas <99250653+KsoraCas@users.noreply.github.com>
2022-02-08 13:32:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# constants.py
-Lab1Height = 180
-Lab1Weight = 90
-Lab1Feet = 4000
-Lab1Time = 120
+# constants
+HEIGHT = 1.80
+WEIGHT = 90
+FEET = 10000
+TIME = 120
 
 # main.py
 
-Step = (Lab1Height / 4) + 0.37
-Length =  Lab1Feet * Step
-Speed = Length / Lab1Time
-Ccal = 0.035 * Lab1Weight + ((Speed ** 2) / Lab1Height) * 0.0029 * Lab1Weight;
+Step = (HEIGHT / 4) + 0.37
+Length =  FEET * Step
+Speed = Length / TIME
+Ccal = 0.035 * WEIGHT + (Speed**2 / HEIGHT) * 0.0029 * WEIGHT;
 
 print (f'Расстояние: {Length} , Калории - {Ccal}')
 #Расстояние: 181480.0 , Калории - 3319.520561111111
+#Расстояние в км: 181.48
+KM = Length/1000
+print (f'Расстояние в км: {KM}')
+
+if KM > 6:
+ print ('Отличная работа!')
+elif KM > 4:
+ print ('Почти выполнил норму!')
+else:
+ print ('Давай еще ходи')
